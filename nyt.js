a9e1e4a52dbceef2e23d823baee818f838e4d811
mhirschmann <mhirschmann@national.aaa.com>
2018-03-10 12:05:43 -0500

    beginning

--- nyt.js
+++ nyt.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 
-
+    var url = "https://api.nytimes.com/svc/archive/v1/1891/12.json";
+    url += '?' + $.param({
+      'api-key': "16b2450d5a3444418fd80c13c69915f2"
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(result) {
+      console.log(result);
+    }).fail(function(err) {
+      throw err;
+    });
 
 
 
