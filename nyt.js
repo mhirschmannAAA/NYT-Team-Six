83d90dabe1e76d5a42be9ab742c70c956c58f855
mhirschmann <mhirschmann@national.aaa.com>
2018-03-10 12:12:14 -0500

    added var

--- nyt.js
+++ nyt.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
 
-    var url = "https://api.nytimes.com/svc/archive/v1/1891/12.json";
+    var year = "1964";
+    var byear = "";
+    var eyear = "";
+    var numArt = 0;
+
+
+    var url = "https://api.nytimes.com/svc/archive/v1/" + year + "/12.json";
     url += '?' + $.param({
       'api-key': "16b2450d5a3444418fd80c13c69915f2"
     });
